d4a7c7fb0859ae35b4497198296391497b80a2c5
theUncleOleh <77876967+theUncleOleh@users.noreply.github.com>
2022-06-04 17:55:07 +0000

    Deploying to gh-pages from @ theUncleOleh/goit-react-hw-08-phonebook@481a251378187d6e549e5830fef2a3c65136ed73 🚀

---
+++ static/js/520.73581350.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{4520:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var r=n(3504),o=n(184);function a(){return(0,o.jsxs)("h1",{children:["404 not found page :( transfer on ",(0,o.jsx)(r.rU,{to:"/",children:"Home page"})]})}}}]);
+//# sourceMappingURL=520.73581350.chunk.js.map
